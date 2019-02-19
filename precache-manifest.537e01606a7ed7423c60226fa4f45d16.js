a743b430282f21076a34561e1b96d5a719782667
zoman_hp <zomanhome@gmail.com>
2019-02-19 16:20:42 +0200

    Updates

---
+++ precache-manifest.537e01606a7ed7423c60226fa4f45d16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "347f3e79576cd40f2dea",
+    "url": "/stardb/static/js/runtime~main.347f3e79.js"
+  },
+  {
+    "revision": "49e7586a585d0e5bd688",
+    "url": "/stardb/static/js/main.49e7586a.chunk.js"
+  },
+  {
+    "revision": "ad3c97eb648d18fd0895",
+    "url": "/stardb/static/js/2.ad3c97eb.chunk.js"
+  },
+  {
+    "revision": "49e7586a585d0e5bd688",
+    "url": "/stardb/static/css/main.84a4dae3.chunk.css"
+  },
+  {
+    "revision": "8a86c18e92ff33d4decee4ab3f9b289f",
+    "url": "/stardb/index.html"
+  }
+];
