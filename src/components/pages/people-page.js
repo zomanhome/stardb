81ed1f0304883c456f40fdda1a12ff6cf118948a
zoman_hp <zomanhome@gmail.com>
2019-02-19 16:04:29 +0200

    test

---
+++ src/components/pages/people-page.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { PersonDetails, PersonList } from "../sw-components";
+import Row from "../row";
+import { withRouter } from "react-router-dom";
+
+const PeoplePage = ({ match, history }) => {
+  const { id } = match.params;
+  return (
+    <Row
+      left={
+        <PersonList
+          onItemSelected={id => {
+            history.push(id);
+          }}
+        />
+      }
+      right={<PersonDetails itemId={id} />}
+    />
+  );
+};
+export default withRouter(PeoplePage);
